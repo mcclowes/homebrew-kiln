5766c6b8b43e3ce38d16c8ac7583cf45216110d0
Max Clayton Clowes <max@mcclowes.com>
2026-06-12 20:57:56 +0100

    kiln 0.2.1

--- Casks/kiln.rb
+++ Casks/kiln.rb
@@ -1,6 +1,6 @@
 cask "kiln" do
-  version "0.2.0"
-  sha256 "1701034485abdd20ad3c215dc07df91af6e3d103e62d3bbf9d7acef0f8bf8ecd"
+  version "0.2.1"
+  sha256 "c5d09df2ae967f532ac9752b17c72590fc68e82c956b7e05d8b55ec6714b75c4"
 
   url "https://github.com/mcclowes/homebrew-kiln/releases/download/v#{version}/Kiln-#{version}.zip"
   name "Kiln"
@@ -13,7 +13,7 @@ cask "kiln" do
   app "Kiln.app"
 
   zap trash: [
-    "~/Library/Application Support/destill",
+    "~/Library/Application Support/kiln",
     "~/Library/Preferences/com.mcclowes.kiln.plist",
     "~/Library/Saved Application State/com.mcclowes.kiln.savedState",
   ]
